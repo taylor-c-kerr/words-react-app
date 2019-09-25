f6513d515adf5ddc2a18ffbe8b5148342fe5d737
Taylor Kerr <tckerr14@gmail.com>
2019-09-25 15:25:11 -0400

    feature/tile-styling add styling to components

--- src/components/Tile/components/Name/index.js
+++ src/components/Tile/components/Name/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 
 class Name extends React.Component {
   constructor(props) {
@@ -7,7 +8,7 @@ class Name extends React.Component {
   
   render() {
     return (
-      <div>
+      <div className='name'>
         Name: {this.props.name}
       </div>
     );
