570aae288595753573b842089fa5a4abd3ac7bb2
Taylor Kerr <tckerr14@gmail.com>
2019-09-25 14:40:27 -0400

    feature/set-up-service display data fetched from backend and an example Tile component with hard-coded props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WordsApi from './services/WordsApi/index';
+import Tile from './components/Tile/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      words: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const words = await WordsApi.getWords();
+      this.setState({
+        isLoaded: true,
+        error: null,
+        words: words.data
+      })
+    }
+    catch (error) {
+      this.setState({
+        error: error
+      })
+    }
+  }
+
+  
+  render() {
+    const { words, isLoaded, error } = this.state;
+    if (!isLoaded) {
+      return <div className="App">LOADING...</div>
+    }
+    if (error) {
+      return <div className="App">{error}</div>
+    }
+    return (
+      <div className="App">
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>Thoughtful Moose</div>
+        <br />
+        From database:
+        {words.map(word => {
+          return <div>{`id: ${word.id}, name: ${word.name}, definition: ${word.definition}`}</div>
+        })}
+        <br />
+        From component:
+        <Tile name="Pineapple" definition="yellow spikey thing" />
+      </div>
+    );
+
+  }
 }
 
 export default App;
