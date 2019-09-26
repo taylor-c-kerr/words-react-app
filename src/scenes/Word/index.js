dcdd26b897213ff47852577242cac7cc61e6559d
Taylor Kerr <tckerr14@gmail.com>
2019-09-26 18:32:39 -0400

    feature/view-word display basic word component

--- src/scenes/Word/index.js
+++ src/scenes/Word/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import WordsApi from '../../services/WordsApi/index';
 
 class Word extends React.Component {
 	constructor(props) {
 	  super(props);
+	  this.state = {
+	  	isLoaded: false,
+	  	word: null,
+	  	error: false
+	  }
 	}
 
 	async componentDidMount() {
 	  try {
-	    const word = await WordsApi.getWord(this.props.match.params.id);
+	  	const {id} = this.props.match.params;
+	    const word = await WordsApi.getWord(id);
 	    this.setState({
 	      isLoaded: true,
-	      error: null,
-	      words: words.data
+	      error: false,
+	      word: word.data
 	    })
 	  }
 	  catch (error) {
@@ -22,8 +29,18 @@ class Word extends React.Component {
 	}
 
 	render() {
-		const {id} = this.props.match.params
-		return <div>You are looking at {id}</div>
+		const {word, isLoaded, error} = this.state;
+
+		if (!isLoaded) {
+		  return <div>LOADING...</div>
+		}
+		if (error) {
+		  return <div>{error}</div>
+		}
+
+		return <div>
+			<h2>{word.name}</h2>
+		</div>
 	}
 }
 
