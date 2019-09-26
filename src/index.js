8385261f186aff4fc9f28956d67363c057f747c1
Taylor Kerr <tckerr14@gmail.com>
2019-09-26 18:14:01 -0400

    feature/view-word reference App scene in the main index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './scenes/Home/index';
+import App from './scenes/App/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
