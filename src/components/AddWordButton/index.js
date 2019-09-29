52c56cad479681c76e3a0dd5cf3c7a0f0cca0a54
Taylor Kerr <tckerr14@gmail.com>
2019-09-29 11:34:32 -0400

    feature/add-word create a button on the home page that will take you to the add word page when clicked on

---
+++ src/components/AddWordButton/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+class AddWordButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isClicked: false
+		}
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		this.setState({isClicked: true})
+	}
+	render() {
+		if (this.state.isClicked) {
+			return <Redirect push to='/add' />
+		}
+		return <div>ADD WORD</div>
+	}
+}
