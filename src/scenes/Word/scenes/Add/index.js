64d1bd47389f4667ee520ace30579c05a4986c25
Taylor Kerr <tckerr14@gmail.com>
2019-09-29 22:03:18 -0400

    feature/add-word set the value of state.definition as an array rather than a string

--- src/scenes/Word/scenes/Add/index.js
+++ src/scenes/Word/scenes/Add/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Input from '../../../../components/Input/index';
 import Button from '../../../../components/Button/index';
+import WordsApi from '../../../../services/WordsApi/index';
+import uuidv4 from 'uuid/v4';
+import {Redirect} from 'react-router-dom'
 
 class Add extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			id: '',
 			name: '',
-			definition: []
+			definition: [],
+			isSubmitted: false
 		}
 
 		this.getInputValue = this.getInputValue.bind(this);
@@ -15,14 +20,35 @@ class Add extends React.Component {
 	}
 
 	getInputValue(value) {
-		this.setState(value);
+		if (value.definition) {
+			// this.setState({definition: [value.definition]})
+			console.log([value.definition]);
+			value.definition = [value.definition];
+		}
+			this.setState(value);
+		
 	}
 
-	handleClick(e) {
-		alert('SUBMITTED!');
+	async handleClick(e) {
+		try {
+			await WordsApi.postWord(this.state);
+			this.setState({isSubmitted: true})
+			
+		}
+		catch (error) {
+			alert(error)
+		}
 	}
 
 	render() {
+		if (!this.state.id) {
+			this.setState({id: uuidv4()})
+		}
+
+		if (this.state.isSubmitted) {
+			return <Redirect push to='/' />;
+		}
+
 		return (
 			<div onSubmit={this.handleSubmit}>
 				<Input name='name' value={this.getInputValue}/>
