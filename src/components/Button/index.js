e67558d37ca6a232facf47d6de4c67046c9b26c7
Taylor Kerr <tckerr14@gmail.com>
2019-10-17 16:17:52 -0400

    feature/add-basic-styling add in some basic styling

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Btn from 'react-bootstrap/Button';
 
 class Button extends React.Component {
 	constructor(props) {
@@ -15,12 +18,18 @@ class Button extends React.Component {
 		this.setState({
 			isClicked: true
 		})
-		this.props.onClick()
 	}
 
 	render() {
+		const {value, variant} = this.props;
 		return (
-			<button onClick={this.handleClick} type='button'>{this.props.value}</button>
+			// <button onClick={this.handleClick} type='button'>{this.props.value}</button>
+			<Btn 
+				onClick={this.handleClick}
+				variant={[variant || 'primary']}
+			 >
+			{value}
+			</Btn>
 		)
 	}
 
