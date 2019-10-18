ae0cb0634d3bb8514e4dc3e42ca234e4a1c52044
Taylor Kerr <tckerr14@gmail.com>
2019-10-17 21:40:39 -0400

    feature/add-basic-styling remove unused components

--- src/components/AddWordButton/AddWordButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddWordButton from './index';
-
-it('renders without crashing', () => {
-const div = document.createElement('div');
-ReactDOM.render(<AddWordButton/>, div);
-ReactDOM.unmountComponentAtNode(div);
-});
--- src/components/CloseButton/CloseButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CloseButton from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CloseButton/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
