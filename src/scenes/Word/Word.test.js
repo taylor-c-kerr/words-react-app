de63d0ae03f87a111a225676bddec24774839850
Taylor Kerr <tckerr14@gmail.com>
2019-12-03 20:54:35 -0500

    fix Definition test add PartOfSpeech test

--- src/scenes/Word/Word.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Word from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Word />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
